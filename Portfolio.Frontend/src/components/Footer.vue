013eda734f69165ad2d5b520045eab3971e000f2
Gheorghe Balan <gba@planday.com>
2019-07-08 09:06:57 +0200

    fixed CV button

--- Portfolio.Frontend/src/components/Footer.vue
+++ Portfolio.Frontend/src/components/Footer.vue
@@ -3,7 +3,7 @@
   <div class="grid-container">
     <div class="grid-x grid-margin-x align-center">
       <div class="cell medium-3 text-center footer-col">
-         <p>  <button class="button primary" :href="cv" target="_blank">{{cvText}}</button></p>
+         <p>  <a class="button primary" :href="cv" target="_blank">{{cvText}}</a></p>
           <p> <a href="https://www.linkedin.com/in/nerija-butkevi%C4%8Di%C5%ABt%C4%97-balan-41176445/" target="_blank"> LinkedIn</a> 
             | <a href="https://www.behance.net/nerijabalan_ux" target="_blank"> Behance</a> 
             </p>
